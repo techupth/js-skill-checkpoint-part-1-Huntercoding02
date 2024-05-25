5edefdf4ee1aa059b9432955e43cf7366b877e31
Hunter <hunvicter@gmail.com>
2024-05-26 00:04:38 +0700

    hunter/solved-q.2

--- question-2.js
+++ question-2.js
@@ -6,6 +6,14 @@ const inventory = [
 // เริ่มเขียนโค้ดตรงนี้
 inventory[0].quantity = 200
 inventory.push({ name: "Orange", price: 20, quantity: 300 })
+
+const totalSum = inventory.reduce((acc,curr)=>{
+  console.log(acc);
+  console.log(curr);
+  return acc+(curr.price*curr.quantity)
+},0)
+console.log(totalSum);
+/*
 let applePrice = inventory[0].price * inventory[0].quantity
 let bananaPrice = inventory[1].price * inventory[1].quantity
 let orangePrice = inventory[2].price * inventory[2].quantity
@@ -13,6 +21,6 @@ console.log(applePrice);
 console.log(bananaPrice);
 console.log(orangePrice);
 let totalSum = applePrice + bananaPrice + orangePrice
-
+*/
 console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalSum} บาท`);
 
